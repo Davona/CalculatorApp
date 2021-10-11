adfcf148fc5748feddd018a67cc62f89debe776d
Davith Shamiryan <dshamiryan@list.ru>
2021-10-11 18:50:00 +0400

    Add project files.

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public static int Sum(int num1,int num2) 
+        {
+            return num1 + num2;
+        }
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multip(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divis(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static void Result(int result) 
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result =Calculator.Sum(25, 17);
+            Calculator.Result(result);
+            int result1=Calculator.Sub(100, 45);
+            Calculator.Result(result1);
+            int result2=Calculator.Multip(8, 12);
+            Calculator.Result(result2);
+            int result3=Calculator.Divis(80, 4);
+            Calculator.Result(result3);
+        }
+    }
+}
